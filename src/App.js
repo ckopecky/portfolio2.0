f9ebc4e8d5fb5764bb05ade0b477aaa91452b7ba
ckopecky <kopecky12112@gmail.com>
2020-04-22 03:10:59 -0700

    Add styled global style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Hero } from './components/Hero';
+import { ThemeContext } from './context/ThemeContext';
+import Navbar from './components/Navbar';
+import { AppContext } from './context/AppContext';
+import Grid from './components/Grid';
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    height: 100%;
+    margin: 0 auto;
+    font-size: 1.4rem;
+    width: 100%;
+    background-image: ${(props) => {
+		  return props.theme.backgroundPicture;
+    }};
+    background-attachment: fixed;
+    background-size: cover;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* background: ${props => props.theme.background}; */
+    color: ${props => props.theme.onBackground};
+    z-index: 0;
+  }
+
+  a {
+    color: ${props => props.theme.onSurface};
+    text-decoration: none;
+    &:hover {
+      color: ${props => props.theme.secondaryVariant}
+    }
+  }
+
+  button {
+    color: ${props => props.theme.onSecondary};
+    background: ${props => props.theme.oppositeTheme === "dark" ? props.theme.secondary : props.theme.secondaryVariant};
+    cursor: pointer;
+    padding: 5px 20px;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+`;
+
+const OverlayStyle = styled.div`
+    background-color: ${(props) => {
+      return props.theme.oppositeTheme === 'dark'
+        ? 'rgba(218, 200, 175, 0.5)'
+        : 'rgba(0, 0, 102, 0.8)';
+	  }};
+    background-attachment: fixed;
+    width: 100%;
+
+
+`;
+
+
+const App = () => {
+  const { theme } = useContext(ThemeContext);
+  const { isSticky, stickyRef } = useContext(AppContext);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <OverlayStyle theme={theme}>
+        <GlobalStyle theme={theme}/>
+        <Navbar sticky={isSticky}/>
+        <Hero stickyRef={stickyRef}/>
+        <Grid />
+      </OverlayStyle>
+    </React.Fragment>
   );
 }
 
